5c5ae713a7b0595febe6bd0ddff958defca44272
yokomaru <cabochetoganlan@gmail.com>
2026-05-30 17:47:23 +0900

    Time.zoneを'Asia/Tokyo'に設定

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module CitytoriProto
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Asia/Tokyo'
   end
 end
