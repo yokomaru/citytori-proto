76441e0b4be82c81074cb4588d02b5435f464d17
yokomaru <cabochetoganlan@gmail.com>
2026-05-30 17:47:03 +0900

    is_publicを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,29 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_05_29_163630) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_30_075938) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.bigint "record_id", null: false
+    t.string "record_type", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
     t.bigint "byte_size", null: false
     t.string "checksum"
+    t.string "content_type"
     t.datetime "created_at", null: false
+    t.string "filename", null: false
+    t.string "key", null: false
+    t.text "metadata"
+    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
@@ -43,22 +43,23 @@ ActiveRecord::Schema[7.1].define(version: 2026_05_29_163630) do
   end
 
   create_table "word_chain_walk_steps", force: :cascade do |t|
-    t.string "word"
-    t.text "memo"
+    t.datetime "created_at", null: false
     t.integer "index"
     t.decimal "latitude", precision: 10, scale: 6
     t.decimal "longitude", precision: 10, scale: 6
-    t.bigint "word_chain_walk_id", null: false
-    t.datetime "created_at", null: false
+    t.text "memo"
     t.datetime "updated_at", null: false
+    t.string "word"
+    t.bigint "word_chain_walk_id", null: false
     t.index ["word_chain_walk_id"], name: "index_word_chain_walk_steps_on_word_chain_walk_id"
   end
 
   create_table "word_chain_walks", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "finished_at"
+    t.boolean "is_public", default: false, null: false
     t.string "start_char"
     t.datetime "started_at"
-    t.datetime "finished_at"
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
