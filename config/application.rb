aa0c562a785b3b8ac4e418246429631acf87e922
yokomaru <cabochetoganlan@gmail.com>
2026-06-05 19:36:18 +0900

    Rspecの導入とtestディレクトリの削除

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,13 @@ module CitytoriProto
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.time_zone = 'Asia/Tokyo'
+
+    config.generators do |g|
+      g.test_framework :rspec,
+      fixtures: false,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false
+    end
   end
 end
--- test/models/word_chain_walk_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class WordChainWalkTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
