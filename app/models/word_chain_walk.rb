ca2b0263f2d02c410903beea3e55eae3e1c59ab3
yokomaru <cabochetoganlan@gmail.com>
2026-06-07 18:09:59 +0900

    バリデーション追加

--- app/models/word_chain_walk.rb
+++ app/models/word_chain_walk.rb
@@ -1,3 +1,10 @@
 class WordChainWalk < ApplicationRecord
   has_many :word_chain_walk_steps, dependent: :destroy
+
+  validates :start_char, presence: true
+  validates :start_char, length: { is: 1 }
+  validates :start_char, format: { with: /\A[あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわを]*\z/ }
+
+  validates :started_at, presence: true
+  validates :finished_at, comparison: { greater_than: :started_at }, allow_nil: true
 end
