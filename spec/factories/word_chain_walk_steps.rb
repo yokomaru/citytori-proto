d89ed2fab67f69e68ace7a723816d250666bc7db
yokomaru <cabochetoganlan@gmail.com>
2026-06-09 15:30:47 +0900

    画像ありのtraitを追加 - 画像無しバージョンのテストを行うためにtraitとして画像ありを用意した - sample画像を用意

--- spec/factories/word_chain_walk_steps.rb
+++ spec/factories/word_chain_walk_steps.rb
@@ -2,5 +2,15 @@ FactoryBot.define do
   factory :word_chain_walk_step do
     word { "あいうえお" }
     association :word_chain_walk
+
+    trait :with_image do
+      after(:build) do |step|
+        step.image.attach(
+          io: File.open(Rails.root.join("spec/fixtures/files/480x320.png")),
+          filename: "sample.png",
+          content_type: "image/png"
+        )
+      end
+    end
   end
 end
