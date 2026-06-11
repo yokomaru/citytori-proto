0a4722fee940fb91849f6ecd7cc08d83bcbab39a
yokomaru <cabochetoganlan@gmail.com>
2026-06-12 02:57:51 +0900

    終了動線のRequest Specを追加

---
+++ spec/requests/word_chain_walks/completions_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "WordChainWalks::Completions", type: :request do
+  describe "PATCH  /word_chain_walks/:word_chain_walk_id/completion" do
+    let(:word_chain_walk) { FactoryBot.create(:word_chain_walk, start_char: "り") }
+
+    it "散歩が終了すること" do
+      expect do
+        patch word_chain_walk_completion_path(word_chain_walk)
+      end.to change { word_chain_walk.reload.finished? }.from(false).to(true)
+
+      expect(response).to redirect_to(word_chain_walk_completion_path(word_chain_walk))
+    end
+  end
+end
