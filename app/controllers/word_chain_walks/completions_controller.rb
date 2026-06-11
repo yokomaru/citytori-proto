af52ea8f800e51a9ead805c394621ef19e8a6163
yokomaru <cabochetoganlan@gmail.com>
2026-06-12 03:28:02 +0900

    不要なコメントや重複箇所を削除

--- app/controllers/word_chain_walks/completions_controller.rb
+++ app/controllers/word_chain_walks/completions_controller.rb
@@ -5,8 +5,6 @@ class WordChainWalks::CompletionsController < ApplicationController
   end
 
   def update
-    logger.debug "hi!"
-    logger.debug "@word_chain_walk: #{@word_chain_walk}"
     @word_chain_walk = WordChainWalk.find(params[:word_chain_walk_id])
     if @word_chain_walk.update(finished_at: Time.zone.now)
         redirect_to word_chain_walk_completion_path(word_chain_walk_id: @word_chain_walk), notice: "Word chain walk was successfully updated.", status: :see_other
