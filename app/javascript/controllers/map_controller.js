72a48198bf59cb1251e587a662dd2f276e491eac
yokomaru <cabochetoganlan@gmail.com>
2026-06-14 13:44:07 +0900

    吹き出してwordを表示させる

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -3,17 +3,18 @@ import L from 'leaflet'
 
 // Connects to data-controller="map"
 export default class extends Controller {
-  static targets = ["placeholder", "latitude", "longitude"]
+  static targets = ["placeholder", "latitude", "longitude", "word"]
 
   connect() {
-    import("leaflet").then(L => {
+      import("leaflet").then(L => {
         this.map = L.map(this.placeholderTarget).setView([this.latitudeTarget.textContent, this.longitudeTarget.textContent], 18);
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 20,
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(this.map);
-        L.marker([this.latitudeTarget.textContent, this.longitudeTarget.textContent]).addTo(this.map);
-    });
+        let marker = L.marker([this.latitudeTarget.textContent, this.longitudeTarget.textContent]).addTo(this.map);
+        marker.bindPopup(this.wordTarget.textContent, {autoClose:false}).openPopup();
+      });
   }
 
   disconnect() {
