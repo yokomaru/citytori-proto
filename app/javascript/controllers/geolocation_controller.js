837ec2ba6d058cde9e9caa3583d19834ae8a0f5f
yokomaru <cabochetoganlan@gmail.com>
2026-06-18 15:18:43 +0900

    jsのフォーマットを修正

--- app/javascript/controllers/geolocation_controller.js
+++ app/javascript/controllers/geolocation_controller.js
@@ -1,34 +1,38 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="geolocation"
 export default class extends Controller {
-  static targets = ["position", "status", "latitude", "longitude"]
+  static targets = ["position", "status", "latitude", "longitude"];
 
-  success(position){
+  success(position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
-    console.log(position)
+    console.log(position);
 
     this.statusTarget.textContent = "";
     this.positionTarget.textContent = `緯度: ${latitude}°、経度: ${longitude}°`;
-    this.latitudeTarget.value = latitude
-    this.longitudeTarget.value = longitude
+    this.latitudeTarget.value = latitude;
+    this.longitudeTarget.value = longitude;
   }
 
   // 失敗した時の処理
   // エラーメッセージを返す
-  error(err){
+  error(err) {
     console.warn(`ERROR(${err.code}): ${err.message}`);
-    this.statusTarget.textContent = "位置情報取得できませんでした"
+    this.statusTarget.textContent = "位置情報取得できませんでした";
   }
 
   // ボタンを押した時の処理
   fetchPosition() {
     if (!navigator.geolocation) {
-      this.statusTarget.textContent = "このブラウザーは位置情報に対応していません";
+      this.statusTarget.textContent =
+        "このブラウザーは位置情報に対応していません";
     } else {
       this.statusTarget.textContent = "位置情報を取得中…";
-      navigator.geolocation.getCurrentPosition((position) => this.success(position), (error) => this.error(error));
+      navigator.geolocation.getCurrentPosition(
+        (position) => this.success(position),
+        (error) => this.error(error),
+      );
     }
   }
 }
--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,23 +1,32 @@
-import { Controller } from "@hotwired/stimulus"
-import L from 'leaflet'
+import { Controller } from "@hotwired/stimulus";
+import L from "leaflet";
 
 // Connects to data-controller="map"
 export default class extends Controller {
-  static targets = ["placeholder", "latitude", "longitude", "word"]
+  static targets = ["placeholder", "latitude", "longitude", "word"];
 
   connect() {
-      import("leaflet").then(L => {
-        this.map = L.map(this.placeholderTarget).setView([this.latitudeTarget.textContent, this.longitudeTarget.textContent], 18);
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 20,
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(this.map);
-        let marker = L.marker([this.latitudeTarget.textContent, this.longitudeTarget.textContent]).addTo(this.map);
-        marker.bindPopup(this.wordTarget.textContent, {autoClose:false}).openPopup();
-      });
+    import("leaflet").then((L) => {
+      this.map = L.map(this.placeholderTarget).setView(
+        [this.latitudeTarget.textContent, this.longitudeTarget.textContent],
+        18,
+      );
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 20,
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(this.map);
+      let marker = L.marker([
+        this.latitudeTarget.textContent,
+        this.longitudeTarget.textContent,
+      ]).addTo(this.map);
+      marker
+        .bindPopup(this.wordTarget.textContent, { autoClose: false })
+        .openPopup();
+    });
   }
 
   disconnect() {
-      this.map.remove()
+    this.map.remove();
   }
 }
